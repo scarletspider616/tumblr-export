2ec659e7590917bdbbc3c807fc2ade9db75ee201
Joey-Michael Fallone <jm@sp0d3rm0b1le.local>
2018-09-01 17:55:51 -0600

    Add the ability to initialize the television

--- TumblrScraper.py
+++ TumblrScraper.py
@@ -14,43 +14,52 @@ class TumblrScraper:
 	'''
 
 	def __init__(self, filename="options.json"):
-		self.filename = filename
+		self._filename = filename
 
 	''' TumblrScraper Initializer
 
-	Parses the 
+	Parses the config file for login and blog details
+	creates pytumblr client member var _client
 	'''
 
 	def initialize(self):
 		self._get_credentials()
 		self._get_blog_url()
+		self._client = pytumblr.TumblrRestClient(
+			self._consumer_key,
+			self._consumer_secret,
+			self._token,
+			self._token_secret)
 
 	''' PRIVATE Parse credentials from json file referenced in the constructor
 
-	saves self.consumer_key, self.consumer_secret to member vars
+	saves self._consumer_key, self._consumer_secret to member vars
+	save self._token, self._token_secret
 
 	'''
 	def _get_credentials(self):
-		with open(self.filename, "r") as file:
+		with open(self._filename, "r") as file:
 			file_options = json.load(file)
-		self.consumer_key = file_options['consumer_key']
-		self.consumer_secret = file_options['consumer_secret']
-
+		self._consumer_key = file_options['consumer_key']
+		self._consumer_secret = file_options['consumer_secret']
+		self._token = file_options['token']
+		self._token_secret = file_options['token_secret']
 
 	''' PRIVATE Parse blog url from json file referenced in the constructor 
 
 
-	saves blog_url from the json file to member var "self.blog"
+	saves blog_url from the json file to member var "self._blog"
 
 	'''
 	def _get_blog_url(self):
-		with open(self.filename, "r") as file:
+		with open(self._filename, "r") as file:
 			file_options = json.load(file)
-		self.blog = file_options['blog_url']
+		self._blog = file_options['blog_url']
 
 
 if __name__ == "__main__":
 	scraper = TumblrScraper()
 	scraper.initialize()
+
 	
 
